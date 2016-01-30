f67b6ec1d7b576b43f3feecc3f7f2e37248bec00
wangsen <wangs@tongxinyiliao.com>
2016-01-30 11:16:00 +0800

    Initial Commit

---
+++ iOS-oc-wechat/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iOS-oc-wechat
+//
+//  Created by winter on 16/1/30.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iOS-oc-wechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-oc-wechat
+//
+//  Created by winter on 16/1/30.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
