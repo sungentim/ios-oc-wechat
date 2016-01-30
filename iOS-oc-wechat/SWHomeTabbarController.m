0083aca3fe49ccb1b10c33d2b4472e8a34983a2c
wangsen <wangs@tongxinyiliao.com>
2016-01-30 17:54:26 +0800

    添加readme

--- iOS-oc-wechat/SWHomeTabbarController.m
+++ iOS-oc-wechat/SWHomeTabbarController.m
@@ -56,17 +56,28 @@
     
     SWDiscoveryViewController *discoveryCtrl = [[SWDiscoveryViewController alloc] init];
     discoveryCtrl.title = @"发现";
+    discoveryCtrl.tabBarItem.title = @"发现1";
     
     SWMeViewController *meCtrl = [[SWMeViewController alloc] init];
     meCtrl.title = @"我";
+    meCtrl.tabBarItem.title = @"我1";
     
-    SWBaseNavigationController *chatBaseNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:chatCtrl];
-    SWBaseNavigationController *consultBaseNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:consultCtrl];
-    SWBaseNavigationController *discoveryBaseNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:discoveryCtrl];
-    SWBaseNavigationController *meBaseNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:meCtrl];
+    
+    SWBaseNavigationController *chatNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:chatCtrl];
+    chatNavCtrl.tabBarItem.title = @"微信1";
+    chatNavCtrl.tabBarItem.image = [UIImage imageNamed:@"tabbar_chat"];
+    SWBaseNavigationController *consultNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:consultCtrl];
+    consultNavCtrl.tabBarItem.title = @"通讯录1";
+    consultCtrl.tabBarItem.image = [UIImage imageNamed:@"tabbar_contacts"];
+    SWBaseNavigationController *discoveryNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:discoveryCtrl];
+    discoveryNavCtrl.tabBarItem.title = @"发现1";
+    discoveryNavCtrl.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
+    SWBaseNavigationController *meNavCtrl = [[SWBaseNavigationController alloc] initWithRootViewController:meCtrl];
+    meNavCtrl.tabBarItem.title = @"我1";
+    meNavCtrl.tabBarItem.image = [UIImage imageNamed:@"tabbar_me"];
     
     //把VC放到数组中
-    NSMutableArray *controllers = [NSMutableArray arrayWithArray:@[chatBaseNavCtrl,consultBaseNavCtrl,discoveryBaseNavCtrl,meBaseNavCtrl]];
+    NSMutableArray *controllers = [NSMutableArray arrayWithArray:@[chatNavCtrl,consultNavCtrl,discoveryNavCtrl,meNavCtrl]];
     //初始化Tabbarcontroller
     self.viewControllers = controllers;
     self.selectedIndex = 0;
