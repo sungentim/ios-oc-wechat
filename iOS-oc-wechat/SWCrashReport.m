6604da10ca05de94e2a45042bb1129a864492c4e
wangsen <wangs@tongxinyiliao.com>
2016-02-03 16:18:28 +0800

    添加严格单例宏

---
+++ iOS-oc-wechat/SWCrashReport.m
@@ -0,0 +1,19 @@
+//
+//  SWCrashReport.m
+//  iOS-oc-wechat
+//
+//  Created by winter on 16/2/3.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import "SWCrashReport.h"
+
+@implementation SWCrashReport
+
+kSWStrictSingletonForClass(SWCrashReport)
+
++ (void) registerCrashReport {
+    
+}
+
+@end
