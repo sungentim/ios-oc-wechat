c3dbe8fc6022990954dfb61969ae65b810bcd452
wangsen <wangs@tongxinyiliao.com>
2016-02-03 17:24:20 +0800

    发送错误报告邮件（跳到页面外） 还未测试

--- iOS-oc-wechat/SWCrashReport.m
+++ iOS-oc-wechat/SWCrashReport.m
@@ -20,6 +20,15 @@ void UncaughtExceptionHandler(NSException *exception) {
     NSArray *arr = [exception callStackSymbols];//得到当前调用栈信息
     NSString *reason = [exception reason];//非常重要，就是崩溃的原因
     NSString *name = [exception name];//异常类型
-    NSLog(@"程序崩溃信息 \n 异常类型 : %@ \n 崩溃原因 : %@ \n 调用栈信息 : %@", name, reason, arr);
+    
+    NSString *crashLogInfo = [NSString stringWithFormat:@"程序崩溃信息 \n 异常类型 : %@ \n 崩溃原因 : %@ \n 调用栈信息 : %@", name, reason, arr];
+    sendEmailCrashMessage(crashLogInfo);
+    
+}
+
+void sendEmailCrashMessage(NSString *crashLogInfo) {
+    NSString *urlStr = [NSString stringWithFormat:@"mailto://578595193@qq.com?subject=bug报告&body=感谢您的配合! 错误详情:%@",crashLogInfo];
+    NSURL *url = [NSURL URLWithString:[urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    [[UIApplication sharedApplication] openURL:url];
 }
 @end
